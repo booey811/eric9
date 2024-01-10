2f6519ef0e978ec64c9ee958dbc3dae28d59e4f6
booey811 <gabrielbarr811@gmail.com>
2024-01-10 13:53:40 +0000

    updated MainModel

--- app/models/main_item.py
+++ app/models/main_item.py
@@ -10,11 +10,15 @@ class _BaseMainModel(MondayModel):
 	phase_deadline = col_type.DateType(id='date19')
 	description = col_type.TextType(id='text368')
 	motion_task_id = col_type.TextType(id='text76')
+	requested_repairs = col_type.TextType(id='text368')
 
 
 class MainModel(BaseEricModel):
-
 	MONCLI_MODEL = _BaseMainModel
 
 	def __init__(self, main_item_id, moncli_item: moncli.en.Item = None):
 		super().__init__(main_item_id, moncli_item)
+
+	@property
+	def model(self) -> _BaseMainModel:
+		return super().model

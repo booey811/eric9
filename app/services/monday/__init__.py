dac3d7638c19cdac42e3f317c4e3257ac26dc10e
booey811 <gabrielbarr811@gmail.com>
2024-01-10 11:56:33 +0000

    added monday_challenge decorator

--- app/services/monday/__init__.py
+++ app/services/monday/__init__.py
@@ -1,5 +1,8 @@
 import logging
+import json
+from functools import wraps
 
+from flask import request, jsonify
 import moncli
 
 from ... import EricError
@@ -19,6 +22,19 @@ def get_items(item_ids: list, column_values=False):
 	except Exception as e:
 		raise e
 	return items
+
+
+def monday_challenge(func):
+	@wraps(func)
+	def decorated_function(*args, **kwargs):
+		# Check if the incoming request has a 'challenge' key
+		challenge = request.json.get('challenge') if request.json else None
+		if challenge:
+			# If it's a challenge, return the challenge code back to monday.com
+			return jsonify({'challenge': challenge})
+		# Otherwise, proceed with the actual processing
+		return func(*args, **kwargs)
+	return decorated_function
 
 
 class MondayError(EricError):

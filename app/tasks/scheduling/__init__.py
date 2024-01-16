0baf177d31e81197fb0ed5dc5a837e181d5f0bb7
Gabriel Barr <gabrielbarr811@gmail.com>
2024-01-16 21:17:15 +0000

    schedule can print to slack

--- app/tasks/scheduling/__init__.py
+++ app/tasks/scheduling/__init__.py
@@ -1,7 +1,9 @@
 import logging
 import time
 from dateutil.parser import parse
+import os
 
+import config
 from ...services import monday
 from ...services import slack
 from ...models import MainModel
@@ -9,42 +11,55 @@ from ...services.motion import create_task, update_task, list_tasks, delete_task
 from ...utilities import users
 
 log = logging.getLogger('eric')
+conf = config.get_config(os.environ['ENV'])
 
 
-def sync_schedule(user: users.User):
+def sync_schedule(user: users.User, repair_group_id: str = None):
 	"""this will sync motion's current schedule with Monday, and post to slack"""
+
+	if not repair_group_id:
+		repair_group_id = user.repair_group_id
+
 	# get motion schedule
-	schedule = list_tasks(user=user)
+	schedule = list_tasks(user=user)['tasks']
 	edited = False
 	slack_blocks = []
 
-
-	repair_group_items = monday.get_group_items(349212843, user.repair_group_id)
-	repairs = [MainModel(item) for item in repair_group_items]
+	repair_group_items = monday.get_group_items(349212843, repair_group_id)
+	repairs = [MainModel(item.id, item) for item in repair_group_items]
 
 	plotted_task_ids = [repair.model.motion_task_id for repair in repairs]
 	for task in schedule:
 		if task['id'] not in plotted_task_ids:
 			log.debug(f"Found errant task: {task['id']}")
-			delete_task(task['id'], user)
+			delete_task(task['id'], user=user)
 			edited = True
 
 	if edited:
 		seconds = 5
 		log.debug(f"Schedule edited: Waiting for Motion to reschedule {seconds} and re-fetching")
 		time.sleep(seconds)
-		schedule = list_tasks(user=user)
+		schedule = list_tasks(user=user)['tasks']
 
 	# now we actually schedule Monday
 	for task in schedule:
+		start = parse(task['scheduledStart'])
+		end = parse(task['scheduledEnd'])
 		repair = [repair for repair in repairs if repair.model.motion_task_id == task['id']][0]
-		repair.model.phase_deadline = parse(task['scheduledEnd'])
+		repair.model.phase_deadline = end
 		repair.model.save()
-		slack_blocks.append(slack.blocks.add.text_block(f"{repair.model.name}"))
-		slack_blocks.append(slack.blocks.add.text_block(f"{task['scheduledStart']} -> {task['scheduledEnd']}"))
 
+		slack_blocks.append(slack.blocks.add.text_block(f"*{repair.model.name}*"))
+		slack_blocks.append(slack.blocks.add.text_block(f"Hard Deadline: {repair.model.hard_deadline.strftime('%-I:%M')}"))
+		slack_blocks.append(slack.blocks.add.text_block(f"{start.strftime('%-I:%M')} -> {end.strftime('%-I:%M')}"))
 
+	slack.slack_client.chat_postMessage(
+		channel=conf.SLACK_DEV_CHANNEL,
+		text=f'Scheduling Results: {user.name}',
+		blocks=slack_blocks
+	)
 
+	return schedule
 
 
 def sync_monday_with_motion(group_id="new_group26478"):
--- main.py
+++ main.py
@@ -20,30 +20,11 @@ if __name__ == '__main__':
 	elif env == 'testing':
 		pass
 	elif env == 'development':
-		from app.models import ProductModel
-		from app.cache import get_redis_connection, CacheMiss
-		from app.cache.utilities import build_product_cache
-		from app.models.main_item import MainModel
-		from app.services import motion
-		from app.services import monday
-		from pprint import pprint as p
-		from app.services.motion import helpers
-		# main_board = monday.client.get_board(349212843)
-		# dev_group = main_board.get_group(id="new_group99626")
-		# items = monday.get_items([item.id for item in dev_group.items], column_values=True)
-		# for item in items:
-		# 	m = MainModel(item.id, item)
-		# 	print(f"Got {m.model.name}")
-		# 	try:
-		# 		deadline = m.model.hard_deadline.isoformat()
-		# 	except AttributeError:
-		# 		deadline = None
-		# 	print(f"Deadline: {deadline}")
-		# 	r = motion.client.create_task(
-		# 		name=m.model.name,
-		# 		deadline=deadline,
-		# 		description=m.model.description
-		# 	)
+		from app.tasks import scheduling
+		from app.utilities import users
+
+		user = users.get_user('gabe')
+		scheduling.sync_schedule(user, "new_group26478")
 
 	else:
 		raise Exception(f"Invalid ENV: {env}")

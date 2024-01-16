27453dfbb47292a5565e5fa3b4e0f6ce3fdd5a9b
booey811 <gabrielbarr811@gmail.com>
2024-01-16 18:27:54 +0000

    work in progress

--- app/tasks/scheduling/__init__.py
+++ app/tasks/scheduling/__init__.py
@@ -1,16 +1,47 @@
+import logging
 import time
 from dateutil.parser import parse
 
 from ...services import monday
+from ...services import slack
 from ...models import MainModel
-from ...services.motion import create_task, update_task, list_tasks
+from ...services.motion import create_task, update_task, list_tasks, delete_task, MotionError
 from ...utilities import users
+
+log = logging.getLogger('eric')
 
 
 def sync_schedule(user: users.User):
-	"""this will sync motion's current schedule with Monday"""
+	"""this will sync motion's current schedule with Monday, and post to slack"""
 	# get motion schedule
-	schedule = list_tasks(user.motion_assignee_id)
+	schedule = list_tasks(user=user)
+	edited = False
+	slack_blocks = []
+
+
+	repair_group_items = monday.get_group_items(349212843, user.repair_group_id)
+	repairs = [MainModel(item) for item in repair_group_items]
+
+	plotted_task_ids = [repair.model.motion_task_id for repair in repairs]
+	for task in schedule:
+		if task['id'] not in plotted_task_ids:
+			log.debug(f"Found errant task: {task['id']}")
+			delete_task(task['id'], user)
+			edited = True
+
+	if edited:
+		seconds = 5
+		log.debug(f"Schedule edited: Waiting for Motion to reschedule {seconds} and re-fetching")
+		time.sleep(seconds)
+		schedule = list_tasks(user=user)
+
+	# now we actually schedule Monday
+	for task in schedule:
+		repair = [repair for repair in repairs if repair.model.motion_task_id == task['id']][0]
+		repair.model.phase_deadline = parse(task['scheduledEnd'])
+		repair.model.save()
+		slack_blocks.append(slack.blocks.add.text_block(f"{repair.model.name}"))
+		slack_blocks.append(slack.blocks.add.text_block(f"{task['scheduledStart']} -> {task['scheduledEnd']}"))
 
 
 

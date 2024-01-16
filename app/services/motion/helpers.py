42400827c2b60beb11f79ee719755c6b30717b04
booey811 <gabrielbarr811@gmail.com>
2024-01-16 16:28:53 +0000

    removed old sync function

--- app/services/motion/helpers.py
+++ app/services/motion/helpers.py
@@ -51,33 +51,3 @@ def plot_monday_group_to_motion(monday_group_id="new_group49546"):
 		m.model.save()
 	return results
 
-
-def sync_monday(group_id="new_group26478"):
-	main_board = monday.client.get_board(349212843)
-	group = main_board.get_group(id=group_id)
-	items = monday.get_items([item.id for item in group.items], column_values=True)
-	models = [MainModel(item.id, item) for item in items]
-	for model in models:
-		if model.model.motion_task_id:
-			update_task(model.model.motion_task_id, model.model.hard_deadline)
-		else:
-			task = create_task(
-				name=model.model.name,
-				deadline=model.model.hard_deadline,
-				description=model.model.requested_repairs,
-			)
-			model.model.motion_task_id = task['id']
-			model.model.save()
-
-	time.sleep(10)
-	tasks = list_tasks(label='Repair')['tasks']
-	p(tasks)
-	for task in tasks:
-		phase_deadline = parse(task['scheduledEnd'])
-		for model in models:
-			if str(model.model.motion_task_id) == str(task['id']):
-				if model.model.hard_deadline < phase_deadline:
-					model.model.motion_scheduling_status = 'Error'
-				model.model.phase_deadline = phase_deadline
-				model.model.save()
-				break

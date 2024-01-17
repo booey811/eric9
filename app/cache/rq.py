ed8168498ed0f1b647a1540a77858d92feadc2c1
Gabriel Barr <gabrielbarr811@gmail.com>
2024-01-17 20:49:19 +0000

    added basic rq, not sure it's working

---
+++ app/cache/rq.py
@@ -0,0 +1,16 @@
+from rq import Queue
+
+from .redis_client import get_redis_connection
+
+from .. import get_config
+
+conf = get_config()
+a_sync = True
+# if conf.DEBUG:
+#     a_sync = False
+
+queues = {
+    'low': Queue('low', connection=get_redis_connection(), is_async=a_sync),
+    'medium': Queue('medium', connection=get_redis_connection(), is_async=a_sync),
+    'high': Queue('high', connection=get_redis_connection(), is_async=a_sync),
+}

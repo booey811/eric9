52aba7ac84a2cd3089d78b97fd2ace0551bf42a0
Gabriel Barr <gabrielbarr811@gmail.com>
2024-01-18 22:33:07 +0000

    corrected logic for group change check

--- app/routes/scheduling.py
+++ app/routes/scheduling.py
@@ -21,14 +21,20 @@ def handle_repair_group_change():
 	webhook = request.get_data()
 	data = webhook.decode('utf-8')
 	data = json.loads(data)['event']
-	main_id = data['pulseId']
 	new_group_id = data['destGroupId']
 	old_group_id = data['new_group56509']
+	repair_group_ids = [user.repair_group_id for user in users.USER_DATA]
+
+	# if moving from non repair group to non repair group, do nothing
+	if old_group_id not in repair_group_ids and new_group_id not in repair_group_ids:
+		return jsonify({'message': 'Not a repair group'}), 200
+
+	main_id = data['pulseId']
 	item = get_items([main_id])[0]
 	main = MainModel(item)
 	repair_group_ids = [user.repair_group_id for user in users.USER_DATA]
 
-	if old_group_id in repair_group_ids:
+	if old_group_id in repair_group_ids and main.model.motion_task_id:
 		# moving from a repairer group - delete from this repairers schedule
 		user = users.User(repair_group_id=old_group_id)
 		motion = MotionClient(user)
@@ -39,17 +45,19 @@ def handle_repair_group_change():
 		# repair has been moved a repairer's group, add motion task to new repairer schedule
 		user = users.User(repair_group_id=new_group_id)
 		motion = MotionClient(user)
-		if not main.model.hard_deadline:
-			raise scheduling.MissingDeadlineInMonday
+		try:
+			task = motion.create_task(
+				name=main.model.name,
+				deadline=main.model.hard_deadline,
+				description=main.model.requested_repairs,
+				labels=['Repair']
+			)
+		except scheduling.MissingDeadlineInMonday:
+			return jsonify({'message': 'Missing Deadline'}), 200
 
-		task = motion.create_task(
-			name=main.model.name,
-			deadline=main.model.hard_deadline,
-			description=main.model.requested_repairs,
-			labels=['Repair']
-		)
 		main.model.motion_task_id = task['id']
 		main.model.motion_scheduling_status = 'Awaiting Sync'
 		scheduling.schedule_update(new_group_id)
 
 	main.model.save()
+	return jsonify({'message': 'OK'}), 200

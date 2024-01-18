529daf18e8934fdf65628a168e06f6b4cbbca86a
Gabriel Barr <gabrielbarr811@gmail.com>
2024-01-18 22:28:07 +0000

    changed test route

--- app/routes/r_tests.py
+++ app/routes/r_tests.py
@@ -1,9 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 test_bp = Blueprint("test", __name__, url_prefix="/test")
 
 
 @test_bp.route("/")
 def test_index():
-	raise Exception("Forced error")
-	return jsonify({"success": True}), 200
+	return jsonify({"success": True, "path": request.path}), 200

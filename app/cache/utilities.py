6486b241fecbc79e2833fe7754702904c3c4df11
booey811 <gabrielbarr811@gmail.com>
2024-01-30 16:19:38 +0000

    added device cache builder

--- app/cache/utilities.py
+++ app/cache/utilities.py
@@ -2,7 +2,7 @@ import logging
 
 from . import get_redis_connection
 from ..services import monday
-from ..models import ProductModel
+from ..models import ProductModel, DeviceModel
 
 log = logging.getLogger('eric')
 
@@ -28,3 +28,25 @@ def build_product_cache():
 			p.save_to_cache(pipe)
 	pipe.execute()
 	log.info(f"Product cache built")
+
+
+def build_device_cache():
+	log.info("Building Device Cache")
+	devices_board = monday.client.get_board(349212843)  # Devices Board
+	all_simple_items = devices_board.get_items()
+	log.info(f"Fetched {len(all_simple_items)} items (simple)")
+	block_size = 25
+	pipe = get_redis_connection().pipeline()
+	# Cycle through the long list
+	for i in range(0, len(all_simple_items), block_size):
+		# Create a block (slice) for this iteration
+		log.debug(f"Fetching block {i // block_size + 1} of {len(all_simple_items) // block_size + 1}")
+		block = all_simple_items[i:i + block_size]
+		items = monday.get_items([_.id for _ in block], column_values=True)
+		log.info(f"Fetched {len(items)} items fully")
+		for item in items:
+			d = DeviceModel(item.id, item)
+			log.debug(str(d))
+			d.model
+			d.save_to_cache(pipe)
+	pipe.execute()

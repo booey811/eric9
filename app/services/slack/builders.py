84495f627f29debb2c38e6c350ad704f0a23280c
booey811 <gabrielbarr811@gmail.com>
2024-01-31 12:57:55 +0000

    Device View working

--- app/services/slack/builders.py
+++ app/services/slack/builders.py
@@ -1,14 +1,81 @@
+import logging
+
 from .blocks import get_modal_base
 from .blocks.elements import generate_option, generate_option_groups
+from ...models import ProductModel, DeviceModel
+
+log = logging.getLogger('eric')
 
 
 class DeviceAndProductViews:
 
-	def __init__(self):
-		self.slack_data = get_modal_base("Device/Products")
-		self.blocks = self.slack_data['view']['blocks']
+	def __init__(self, device: DeviceModel = None, products=[]):
+		self._device = device
+		self._products = products
+
+		self.view = get_modal_base("Device/Products")
+		self.blocks = self.view['blocks']
 
 	def build_view(self):
-		pass
+		log.debug('Building Device/Products view')
+		self._build_device_select()
+		# self._build_product_select()
 
+		log.debug(self.view)
+		return self.view
 
+	def _build_device_select(self, device_id=None):
+		devices = DeviceModel.query_all()
+		options_dict = {}
+		for device in devices:
+			options_set = options_dict.get(device.device_type)
+			if not options_set:
+				options_set = options_dict[device.device_type] = []
+			options_set.append((device.name, device.id))
+
+		option_groups = generate_option_groups(options_dict)
+
+		self.blocks.append({
+			"type": "input",
+			"block_id": "device_select",
+			"label": {
+				"type": "plain_text",
+				"text": "Select Device",
+				"emoji": True
+			},
+			"element": {
+				"type": "static_select",
+				"action_id": "device_select",
+				"placeholder": {
+					"type": "plain_text",
+					"text": "Select an item",
+					"emoji": True
+				},
+				"option_groups": option_groups
+			}
+		})
+
+	def _build_product_select(self, device_id=None):
+		options = [
+			generate_option(product.name, product.id) for product in ProductModel.query.all()
+		]
+		option_groups = generate_option_groups(options)
+		self.blocks.append({
+			"type": "input",
+			"block_id": "product_select",
+			"label": {
+				"type": "plain_text",
+				"text": "Select Product",
+				"emoji": True
+			},
+			"element": {
+				"type": "static_select",
+				"action_id": "product_select",
+				"placeholder": {
+					"type": "plain_text",
+					"text": "Select an item",
+					"emoji": True
+				},
+				"options": option_groups
+			}
+		})

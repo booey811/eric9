67836e2e46ca573ef23366233d4046cdc3193eff
booey811 <gabrielbarr811@gmail.com>
2024-01-31 12:55:54 +0000

    fixed 'view' nesting

--- app/services/slack/blocks/base.py
+++ app/services/slack/blocks/base.py
@@ -1,22 +1,20 @@
 def get_modal_base(title, submit="Submit", cancel="Cancel"):
 	return {
-		"view": {
-			"type": "modal",
-			"title": {
-				"type": "plain_text",
-				"text": title,
-				"emoji": True
-			},
-			"submit": {
-				"type": "plain_text",
-				"text": submit,
-				"emoji": True
-			},
-			"close": {
-				"type": "plain_text",
-				"text": cancel,
-				"emoji": True
-			},
-			"blocks": []
-		}
+		"type": "modal",
+		"title": {
+			"type": "plain_text",
+			"text": title,
+			"emoji": True
+		},
+		"submit": {
+			"type": "plain_text",
+			"text": submit,
+			"emoji": True
+		},
+		"close": {
+			"type": "plain_text",
+			"text": cancel,
+			"emoji": True
+		},
+		"blocks": []
 	}

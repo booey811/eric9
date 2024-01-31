5ebdf203adc4d3a0ed0df66e9bf81fbc53d9d6c0
booey811 <gabrielbarr811@gmail.com>
2024-01-31 13:42:03 +0000

    modified build_cache task to do products and devices

--- app/tasks/recurring/build_cache.py
+++ app/tasks/recurring/build_cache.py
@@ -1,5 +1,6 @@
 # run_cache.py
-from app.cache.utilities import build_product_cache
+from app.cache.utilities import build_product_cache, build_device_cache
 
 if __name__ == "__main__":
     build_product_cache()
+    build_device_cache()

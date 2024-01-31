40fc5ff04daef0b972a2ea21b77b533cc69f89e9
booey811 <gabrielbarr811@gmail.com>
2024-01-31 12:57:43 +0000

    slack/test working

--- app/routes/slack/quotes.py
+++ app/routes/slack/quotes.py
@@ -1,12 +1,20 @@
 import logging
 
-from ...services.slack import slack_app
+from ...services.slack import slack_app, builders
 
 log = logging.getLogger('eric')
 
 
 @slack_app.command("/test")
-def run_test_function(ack, body):
+def run_test_function(ack, body, client):
 	ack()
 	log.debug("test function ran")
 	log.debug(body)
+	builder = builders.DeviceAndProductViews()
+	view = builder.build_view()
+	log.debug(view)
+	client.views_open(
+		trigger_id=body["trigger_id"],
+		view=view
+	)
+	return True

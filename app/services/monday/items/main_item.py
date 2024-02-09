004396a0b3b7895b8445e0f99ffb324817ca8a53
booey811 <gabrielbarr811@gmail.com>
2024-02-09 14:50:03 +0000

    added MainItem

--- app/services/monday/items/main_item.py
+++ app/services/monday/items/main_item.py
@@ -4,6 +4,26 @@ from ..api import items, columns
 class MainItem(items.BaseItemType):
 	BOARD_ID = 349212843
 
+	# basic info
+	main_status = columns.StatusValue("status4")
+
+	# repair info
+	products_connect = columns.ConnectBoards("board_relation")
+	description = columns.TextValue("text368")
+
+	# tech info
+	technician_id = columns.PeopleValue("person")
+
+	# scheduling info
+	motion_task_id = columns.TextValue("text76")
+	motion_scheduling_status = columns.StatusValue("status_19")
+	hard_deadline = columns.DateValue("date36")
+	phase_deadline = columns.DateValue("date65")
+
+
+class PropertyTestItem(items.BaseItemType):
+	BOARD_ID = 349212843
+
 	text = columns.TextValue('text69')
 	number = columns.NumberValue('dup__of_quote_total')
 	status = columns.StatusValue('status_161')

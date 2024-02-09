86b5be79d8e221375e46e6f6fe4a7ece9f704c9c
booey811 <gabrielbarr811@gmail.com>
2024-02-09 12:47:10 +0000

    added typeform client

---
+++ app/services/typeform/client.py
@@ -0,0 +1,35 @@
+from typeform import Typeform
+
+from ... import conf
+
+
+class TypeformClient:
+	def __init__(self, api_key):
+		self.client = Typeform(api_key)
+
+	def get_responses(
+			self,
+			form_id,
+			since=None,
+			until=None,
+			after=None,
+			before=None,
+			completed=None,
+			sort=None,
+			query=None,
+			fields=None
+	):
+		return self.client.responses.list(
+			uid=form_id,
+			since=since,
+			until=until,
+			after=after,
+			before=before,
+			completed=completed,
+			sort=sort,
+			query=query,
+			fields=fields
+		)
+
+
+conn = TypeformClient(conf.TYPEFORM_API_KEY)

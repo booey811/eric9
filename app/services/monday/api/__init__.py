a210c37f88020d422f30e20bebf5c96f92334297
booey811 <gabrielbarr811@gmail.com>
2024-02-09 14:47:42 +0000

    add get_items_by_group

--- app/services/monday/api/__init__.py
+++ app/services/monday/api/__init__.py
@@ -5,12 +5,13 @@ from .client import MondayAPIError
 
 
 def get_api_items(item_ids):
+	item_ids = [int(_) for _ in item_ids]
 	try:
-		raw = client.conn.items.fetch_items_by_id(item_ids)
+		raw = client.conn.items.fetch_items_by_id(ids=item_ids)
 	except Exception as e:
 		raise MondayAPIError(f"Error calling monday API: {e}")
 
-	if raw.get("errors"):
+	if raw.get("error_message"):
 		raise MondayAPIError(f"Error fetching items from Monday: {raw['errors']}")
 
 	return raw["data"]["items"]
@@ -18,3 +19,15 @@ def get_api_items(item_ids):
 
 def get_board(board_id):
 	boards.get_board(board_id)
+
+
+def get_api_items_by_group(board_id, group_id):
+	try:
+		raw = client.conn.groups.get_items_by_group(board_id, group_id)
+	except Exception as e:
+		raise MondayAPIError(f"Error calling monday API: {e}")
+
+	if raw.get("error_message"):
+		raise MondayAPIError(f"Error fetching items from Monday: {raw['errors']}")
+
+	return raw["data"]["boards"][0]["groups"][0]["items_page"]["items"]

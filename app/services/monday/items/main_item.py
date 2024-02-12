7dad5600c71860476b5c0e3a147ea3ab6f0610dc
booey811 <gabrielbarr811@gmail.com>
2024-02-12 15:02:01 +0000

    added print_stock_check to MainItem

--- app/services/monday/items/main_item.py
+++ app/services/monday/items/main_item.py
@@ -1,4 +1,11 @@
-from ..api import items, columns
+import logging
+
+from ..api import items, columns, get_api_items
+from .product import ProductItem
+from .part import PartItem
+from ....utilities import notify_admins_of_error
+
+log = logging.getLogger('eric')
 
 
 class MainItem(items.BaseItemType):
@@ -19,6 +26,44 @@ class MainItem(items.BaseItemType):
 	motion_scheduling_status = columns.StatusValue("status_19")
 	hard_deadline = columns.DateValue("date36")
 	phase_deadline = columns.DateValue("date65")
+
+	def get_stock_check_string(self):
+		"""
+		Checks stock for all products connected to this main item, and return a string description of them
+		"""
+		log.debug(f"Checking stock for {str(self)}")
+
+		update = '=== STOCK CHECK ===\n'
+		try:
+			in_stock = True
+			product_data = get_api_items(self.products_connect.value)
+			prods = [ProductItem(p['id'], p) for p in product_data]
+			for prod in prods:
+				update += prod.name.upper() + '\n'
+				if not prod.parts_connect.value:
+					message = f"""No parts connected!!!
+					Click to fix my connecting parts to this product by making sure the 'parts' column is filled in
+					https://icorrect.monday.com/boards/2477699024/views/55887964/pulses/{prod.id}"""
+					notify_admins_of_error(message)
+					update += message + '\n'
+				else:
+					parts_data = get_api_items(prod.parts_connect.value)
+					parts = [PartItem(_['id'], _) for _ in parts_data]
+					for part in parts:
+						update += f"{part.name}: {part.stock_level}\n"
+
+			if in_stock:
+				update += "All parts in stock"
+			else:
+				update += "SOME PARTS MAY NOT BE AVAILABLE"
+
+		except Exception as e:
+			log.error(f"Error checking stock for {str(self)}: {e}")
+			notify_admins_of_error(f"Error checking stock for {str(self)}: {e}")
+			update += f"Error checking stock: {e}"
+			raise e
+
+		return update
 
 
 class PropertyTestItem(items.BaseItemType):

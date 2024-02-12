8127d9110a5a6e0772e16e454b14632f4c8539bd
booey811 <gabrielbarr811@gmail.com>
2024-02-12 12:00:02 +0000

    refactored monday client importing

--- app/services/monday/api/__init__.py
+++ app/services/monday/api/__init__.py
@@ -1,4 +1,4 @@
-from . import client
+from .client import conn as monday_connection
 from .items import BaseItemType
 from .boards import cache as boards
 from .exceptions import MondayAPIError
@@ -7,7 +7,7 @@ from .exceptions import MondayAPIError
 def get_api_items(item_ids):
 	item_ids = [int(_) for _ in item_ids]
 	try:
-		raw = client.conn.items.fetch_items_by_id(ids=item_ids)
+		raw = monday_connection.items.fetch_items_by_id(ids=item_ids)
 	except Exception as e:
 		raise MondayAPIError(f"Error calling monday API: {e}")
 
@@ -23,7 +23,7 @@ def get_board(board_id):
 
 def get_api_items_by_group(board_id, group_id):
 	try:
-		raw = client.conn.groups.get_items_by_group(board_id, group_id)
+		raw = monday_connection.groups.get_items_by_group(board_id, group_id)
 	except Exception as e:
 		raise MondayAPIError(f"Error calling monday API: {e}")
 

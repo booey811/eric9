a6c9ebdc2c115ca6c2cfe5d2c49414e1c79e3d69
booey811 <gabrielbarr811@gmail.com>
2024-02-12 17:51:04 +0000

    adapted web bookings for new items

--- app/tasks/monday/web_bookings.py
+++ app/tasks/monday/web_bookings.py
@@ -4,8 +4,6 @@ from zenpy.lib.exception import APIException
 from zenpy.lib.api_objects import Ticket, Comment
 
 from ...services import monday, woocommerce, zendesk
-from ...models.misc import WebBookingModel
-from ...models import ProductModel, MainModel, DeviceModel
 from ...errors import EricError
 from ...utilities import notify_admins_of_error
 
@@ -13,21 +11,18 @@ log = logging.getLogger('eric')
 
 
 def transfer_web_booking(web_booking_item_id):
-	item = monday.get_items([web_booking_item_id])[0]
-	web_booking = WebBookingModel(item.id, item)
-	main = MainModel.MONCLI_MODEL(
-		name=web_booking.model.name,
-		board=monday.client.get_board_by_id(MainModel.BOARD_ID)
-	)
+	item = monday.api.get_api_items([web_booking_item_id])[0]
+	web_booking = monday.items.misc.WebBookingItem(item['id'], item)
+	main = monday.items.MainItem(None)
 
 	# extract Woo Commerce order data
-	woo_order_data = woocommerce.woo.get(f"orders/{web_booking.model.woo_commerce_order_id}")
+	woo_order_data = woocommerce.woo.get(f"orders/{web_booking.woo_commerce_order_id.value}")
 
 	if woo_order_data.status_code != 200:
 		notify_admins_of_error(
-			f"{web_booking} could not find order {web_booking.model.woo_commerce_order_id} in Woo Commerce")
+			f"{web_booking} could not find order {web_booking.woo_commerce_order_id} in Woo Commerce")
 		raise WebBookingTransferError(
-			f"{web_booking} could not find order {web_booking.model.woo_commerce_order_id} in Woo Commerce")
+			f"{web_booking} could not find order {web_booking.woo_commerce_order_id} in Woo Commerce")
 
 	woo_order_data = woo_order_data.json()
 
@@ -79,36 +74,29 @@ def transfer_web_booking(web_booking_item_id):
 		service = 'Walk-In'
 	else:
 		raise WebBookingTransferError(f'{str(web_booking)} could not determine service type')
+
 	main.service = service
 
-	prod_board = monday.client.get_board_by_id(2477699024)
-	search = prod_board.columns['text3']  # woo commerce ID column
-	missing_prods = []
+	search_prod = monday.items.ProductItem(None)
+
+	search_results = []
+
 	try:
-		search_results = prod_board.get_items_by_multiple_column_values(
-			column=search,
-			column_values=[str(_['id']) for _ in repair_products],
-			get_column_values=True
-		)
+		for _ in repair_products:
+			try:
+				result = search_prod.search_board_for_items('woo_commerce_product_id', str(_['id']))[0]
+				search_results.append(monday.items.ProductItem(result['id'], result))
+			except IndexError:
+				log.error(f"{web_booking} could not find Woo product in Eric: {str(_['name'])}({str(_['id'])})")
+
 	except Exception as e:
-		notify_admins_of_error(f"Could Not Get Items by Multiple Values: {e}")
-		search_results = []
-		for woo_prod in repair_products:
-			woo_id = woo_prod['id']
-			search = prod_board.get_column_value('text3')
-			search.value = search.text = str(woo_id)
-			single_result = prod_board.get_items_by_column_value(column_value=search)[0]
-			if single_result:
-				search_results.append(single_result)
-			else:
-				missing_prods.append(woo_prod)
-				notify_admins_of_error(f"Could Not Find {woo_id} in Eric")
+		notify_admins_of_error(f"Could Not Fetch Products for Web Booking: {e}")
 
 	if len(search_results) != len(repair_products):
 		notify_admins_of_error(
-			f"{web_booking} could not find all products in {web_booking.model.woo_commerce_order_id} in Eric")
+			f"{web_booking} could not find all products in {web_booking.woo_commerce_order_id} in Eric")
 
-	products = [ProductModel(_.id, _) for _ in search_results]
+	products = search_results
 	main.products_connect = [str(_.id) for _ in products]
 
 	repair_type = 'Repair'
@@ -116,29 +104,32 @@ def transfer_web_booking(web_booking_item_id):
 		if 'diagnostic' in p.name.lower():
 			repair_type = 'Diagnostic'
 			break
+
 	main.repair_type = repair_type
 
 	device_ids = [p.device_id for p in products if p.device_id is not None]
 	device_id = device_ids[0] if device_ids else None
 	if device_id:
-		device = DeviceModel(device_id)
-		email_subject = f"Your {device.model.name} Repair"
+		d_id = device_id
+		device_data = monday.api.get_api_items([d_id])[0]
+		device = monday.items.DeviceItem(device_data['id'], device_data)
+		email_subject = f"Your {device.name} Repair"
 	else:
-		device = DeviceModel(4028854241)  # defaults to Other Device
+		device_id = 4028854241  # Other Device
 		email_subject = "Your Repair with iCorrect"
 
-	main.device_connect = str(device.id)
+	main.device_id = int(device_id)
 
 	# create zendesk ticket
-	user_search = zendesk.helpers.search_zendesk(web_booking.model.email)
+	user_search = zendesk.helpers.search_zendesk(str(web_booking.email.value))
 	if not user_search:
 		# no user found, create user
 		log.debug(f"No User Found, creating")
 		try:
 			user = zendesk.helpers.create_user(
-				web_booking.model.name,
-				web_booking.model.email,
-				web_booking.model.phone
+				web_booking.name,
+				web_booking.email.value,
+				web_booking.phone.value
 			)
 		except APIException as e:
 			log.debug(f"Could not create user: {e}")
@@ -150,7 +141,7 @@ def transfer_web_booking(web_booking_item_id):
 	else:
 		raise WebBookingTransferError(f"Multiple users found ({len(search_results)}) for {web_booking.model.email}")
 
-	booking_text = f"Website Notes:\n" + web_booking.model.initial_notes
+	booking_text = f"Website Notes:\n" + web_booking.booking_notes.value
 
 	ticket = Ticket(
 		subject=email_subject,
@@ -165,33 +156,30 @@ def transfer_web_booking(web_booking_item_id):
 
 	ticket = zendesk.client.tickets.create(ticket).ticket
 
-	main.email = web_booking.model.email
-	main.phone = web_booking.model.phone
+	main.email = web_booking.email.value
+	main.phone = web_booking.phone.value
 	main.ticket_id = str(ticket.id)
-	main.ticket_url = {
-		'url': f"https://icorrect.zendesk.com/agent/tickets/{ticket.id}",
-		'text': str(ticket.id)
-	}
+	main.ticket_url = [
+		str(ticket.id),
+		f"https://icorrect.zendesk.com/agent/tickets/{ticket.id}",
+	]
 	main.description = "; ".join([f"{p.name}(£{p.price})" for p in products])
 
-	main.point_of_collection = web_booking.model.point_of_collection
-	main.address_comment = web_booking.model.address_comment
-	main.address_street = web_booking.model.address_street
-	main.address_postcode = web_booking.model.address_postcode
-	main.booking_date = web_booking.model.booking_date
+	main.point_of_collection = web_booking.point_of_collection.value
+	main.address_notes = web_booking.address_notes.value
+	main.address_street = web_booking.address_street.value
+	main.address_postcode = web_booking.address_postcode.value
+	main.booking_date = web_booking.booking_date.value
 
 	main.client = 'End User'
 	main.main_status = 'Awaiting Confirmation'
 	main.notifications_status = 'ON'
 
-	main.save()
+	main.create(web_booking.name)
+	main_data = monday.api.get_api_items([main.id])[0]
+	main = monday.items.MainItem(main_data['id'], main_data)
 
-	main = MainModel(main.id, main.item)
-	main.get_thread('notes').add_reply(booking_text)
-	main.print_stock_check()
-
-	if missing_prods:
-		main.get_thread('notes').add_reply(f"Could Not Find WooCommerce References in Products & Pricing\n\n{missing_prods}")
+	main.add_update(main.get_stock_check_string(), main.notes_thread_id.value)
 
 	return main
 

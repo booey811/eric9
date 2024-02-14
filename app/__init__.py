fa6d38a050d620b55c686590c7d0aba56f031484
booey811 <gabrielbarr811@gmail.com>
2024-02-14 10:41:47 +0000

    import blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -39,10 +39,8 @@ def create_app():
 
 	# Here, import and register blueprints
 	from .routes import scheduling, slack, monday as monday_routes, ai as ai_routes
-	# app.register_blueprint(scheduling.scheduling_bp)
-	# app.register_blueprint(r_tests.test_bp)
+	app.register_blueprint(scheduling.scheduling_bp)
 	app.register_blueprint(ai_routes.ai_bp)
-	# app.register_blueprint(monday_routes.main_board.main_board_bp)
 
 	return app
 

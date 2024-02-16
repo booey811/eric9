66fc713d50ac7e5e11c839e9c915ffd9fb1d2d1e
booey811 <gabrielbarr811@gmail.com>
2024-02-16 14:39:48 +0000

    correcting check_run syntax

--- app/services/openai/utils.py
+++ app/services/openai/utils.py
@@ -34,7 +34,7 @@ def check_run(thread_id, run_id, success_endpoint=''):
 		if run.status == 'completed':
 			# post to success endpoint
 			data = run.metadata or {}
-			log.debug(f'Run complete, preparing data for endpoint: {data["endpoint"]}')
+			log.debug(f'Run complete, preparing data for endpoint: {data.get("success_endpoint")}')
 			log.debug(f"Metadata: {run.metadata}")
 			data['thread_id'] = thread_id
 			data['run_id'] = run_id

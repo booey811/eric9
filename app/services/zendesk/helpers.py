d4dae2ec8732e354d11299bf8d519f0b16d4337a
booey811 <gabrielbarr811@gmail.com>
2024-02-20 13:02:35 +0000

    add create_ticket_field_option

--- app/services/zendesk/helpers.py
+++ app/services/zendesk/helpers.py
@@ -1,4 +1,7 @@
 import logging
+import requests
+import json
+import os
 
 from zenpy.lib.api_objects import User
 from zenpy.lib.exception import APIException
@@ -32,3 +35,46 @@ def create_user(name, email, phone):
 	return new_user
 
 
+def create_ticket_field_option(ticket_field_id, option_name, option_tag):
+	"""
+
+	creates a new ticket field option, assigned to the tag provided, in the relevant custom field
+
+	:param ticket_field_id: ID of the custom field to edit
+	:type ticket_field_id: int
+	:param option_name: title of the option to be shown to users
+	:type option_name: str
+	:param option_tag: tag that will be used to control which option is selected
+	:type option_tag:
+	"""
+
+	import base64
+
+	def _encode_auth():
+		plain_str = f"admin@icorrect.co.uk/token:{os.getenv('ZENDESK')}"
+		encoded_bytes = base64.b64encode(plain_str.encode("utf-8"))
+		encoded_string = encoded_bytes.decode()
+		return encoded_string
+
+	HEADERS = {
+		"Content-Type": "application/json",
+		"Authorization": f"Basic {_encode_auth()}"
+	}
+
+	url = f"https://icorrect.zendesk.com/api/v2/ticket_fields/{ticket_field_id}/options"
+
+	data = {
+		'custom_field_option': {
+			"name": option_name,
+			"value": option_tag,
+		}
+	}
+
+	response = requests.request(
+		"POST",
+		url,
+		headers=HEADERS,
+		data=json.dumps(data)
+	)
+
+	return response

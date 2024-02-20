5e6686c4133a09c394a3ef0fd991af3981a792de
booey811 <gabrielbarr811@gmail.com>
2024-02-20 16:48:40 +0000

    add custom fields dict

---
+++ app/services/zendesk/custom_fields.py
@@ -0,0 +1,13 @@
+
+FIELDS_DICT = {
+	"main_status": 360005728837,
+	"service": 360010444117,
+	"repair_type": 360010444077,
+	"client": 360010408778,
+	"passcode": 360005102118,
+	"imeisn": 360004242638,
+	"booking_date_string": 10885002669585,
+	"address_notes": 360006582798,
+	"address_street": 360006582778,
+	"address_postcode": 360006582758,
+}

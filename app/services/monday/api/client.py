caf9f54a55de20b1097a17bab5ae6e1ff54ee8cc
booey811 <gabrielbarr811@gmail.com>
2024-02-20 14:04:21 +0000

    corrected errors for get_api_items

--- app/services/monday/api/client.py
+++ app/services/monday/api/client.py
@@ -13,13 +13,15 @@ conn = monday.MondayClient(conf.MONDAY_KEYS["gabe"])
 
 def get_api_items(item_ids):
 	item_ids = [int(_) for _ in item_ids]
+	if not item_ids:
+		return []
 	try:
 		raw = conn.items.fetch_items_by_id(ids=item_ids)
 	except Exception as e:
 		raise MondayAPIError(f"Error calling monday API: {e}")
 
 	if raw.get("error_message"):
-		raise MondayAPIError(f"Error fetching items from Monday: {raw['errors']}")
+		raise MondayAPIError(f"Error fetching items from Monday: {raw['error_message']}")
 
 	return raw["data"]["items"]
 

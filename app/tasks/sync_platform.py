2b328d423b9a92f6810f63443f68bcb0291ead63
booey811 <gabrielbarr811@gmail.com>
2024-02-21 09:58:43 +0000

    added default values for booking date string

--- app/tasks/sync_platform.py
+++ app/tasks/sync_platform.py
@@ -118,19 +118,30 @@ def sync_to_zendesk(main_item_id, ticket_id):
 			)
 
 		# booking date string
+		ds = "as soon as possible"
 		if main_item.booking_date.value:
 			try:
-				# format: "12:30PM on Wednesday 14 February"
-				ds = main_item.booking_date.value.strftime("%I:%M%p on %A %d %B")
+				if not main_item.service.value:
+					raise MondayDataError("Cannot convert Booking Date to string, please select which service the client will be using")
+				if 'mail' in main_item.service.value.lower():
+					# mail format: on Wednesday 14th February
+					ds = main_item.booking_date.value.strftime("on %A %d %B")
+				else:
+					# courier format: "at 12:30PM on Wednesday 14 February"
+					ds = main_item.booking_date.value.strftime("at %I:%M%p on %A %d %B")
+			except MondayDataError as e:
+				failed.append(['BookingDateString (Service Issue)', main_item.service.value])
+				raise e
 			except Exception as e:
 				failed.append(['BookingDateString', main_item.booking_date.value])
+				notify_admins_of_error(str(e))
 				raise e
-			ticket.custom_fields.append(
-				CustomField(
-					id=field_ids['booking_date_string'],
-					value=ds
-				)
+		ticket.custom_fields.append(
+			CustomField(
+				id=field_ids['booking_date_string'],
+				value=ds
 			)
+		)
 
 		if failed:
 			message = f"{str(main_item)}Failed Monday -> Zendesk sync, could not locate labels\n\n"

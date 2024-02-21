91e4db85dcad64faefd6284db2f78a4c6a03d083
booey811 <gabrielbarr811@gmail.com>
2024-02-21 18:36:51 +0000

    re-corrected main.py

--- main.py
+++ main.py
@@ -17,18 +17,6 @@ if __name__ == '__main__':
 	elif env == 'testing':
 		eric.run()
 	elif env == 'development':
-		from app.tasks.sync_platform import sync_to_zendesk
-		from app.tasks.monday import sessions
-		w_id = 6017161322
-		i_id = 6099674053
-		t_id = 25625
-
-		phase_model_id = 6106627585
-
-		import datetime
-		from app.tasks.monday.web_bookings import transfer_web_booking
-		# notifications.notify_zendesk.send_macro(i_id)
-		from app.services import zendesk
-
+		pass
 	else:
 		raise Exception(f"Invalid ENV: {env}")

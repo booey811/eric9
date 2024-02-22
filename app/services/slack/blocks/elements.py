efcda0f5d49008064b08d5cfae2a81e8abb75c72
booey811 <gabrielbarr811@gmail.com>
2024-02-22 16:56:37 +0000

    options working nicely

--- app/services/slack/blocks/elements.py
+++ app/services/slack/blocks/elements.py
@@ -1,5 +1,3 @@
-
-
 def external_select_element(action_id, placeholder, min_query_length=3):
 	return {
 		"action_id": str(action_id),
@@ -7,6 +5,35 @@ def external_select_element(action_id, placeholder, min_query_length=3):
 		"placeholder": {
 			"type": "plain_text",
 			"text": placeholder
-		  },
+		},
 		"min_query_length": min_query_length
 	}
+
+
+def multi_external_select_element(action_id, placeholder, min_query_length=3):
+	return {
+		"action_id": str(action_id),
+		"type": "multi_external_select",
+		"placeholder": {
+			"type": "plain_text",
+			"text": placeholder
+		},
+		"min_query_length": min_query_length
+	}
+
+
+def rich_text_elements(list_of_content):
+	assert isinstance(list_of_content, list), "list_of_content must be a list of dictionaries"
+
+	def make_element(text):
+		return {
+			"type": "text",
+			"text": text
+		}
+
+	basic = {
+		"type": "rich_text_section",
+		"elements": [make_element(_) for _ in list_of_content]
+	}
+
+	return [basic]

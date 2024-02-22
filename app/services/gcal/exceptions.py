b1f7eb171f977d0c054ceed686efe2ec5722e374
booey811 <gabrielbarr811@gmail.com>
2024-02-22 11:33:41 +0000

    correct syntax for gcal error

--- app/services/gcal/exceptions.py
+++ app/services/gcal/exceptions.py
@@ -4,4 +4,4 @@ from ...errors import EricError
 class GCalAPIError(EricError):
 
 	def __str__(self):
-		return f"GCal Returned A HTTP Error: {str(self.e)}"
+		return f"GCal Returned A HTTP Error: {str(self.message)}"

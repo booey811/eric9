6d50c17d044cf2fc0be5d72b5c7dfd3111b9da2e
booey811 <gabrielbarr811@gmail.com>
2024-02-22 13:51:37 +0000

    edited notify of error

--- app/utilities/__init__.py
+++ app/utilities/__init__.py
@@ -7,10 +7,27 @@ def notify_admins_of_error(trace):
 	from ..services.slack import blocks, slack_client
 	# Integrate with an error notification tool (e.g., email, Slack, PagerDuty)
 	# This function is where you would include your custom notification logic
+
+	def make_block(content):
+		return {
+			"type": "rich_text",
+			"elements": [
+				{
+					"type": "rich_text_section",
+					"elements": [
+						{
+							"type": "text",
+							"text": content
+						}
+					]
+				}
+			]
+		}
+
 	s_blocks = [
-		blocks.add.text_block("Error"),
-		blocks.add.text_block(trace)
+		make_block(f"An error occurred in the application: {trace}")
 	]
+
 	slack_client.chat_postMessage(
 		channel=config.get_config().SLACK_ERROR_CHANNEL,
 		text='eric9:Unhandled Error',

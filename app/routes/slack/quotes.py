c9be5e6a8a8c988c4a7202bc7a32db9f523fd371
booey811 <gabrielbarr811@gmail.com>
2024-02-22 12:17:07 +0000

    added test route for Slack

--- app/routes/slack/quotes.py
+++ app/routes/slack/quotes.py
@@ -1,22 +1,23 @@
-# import logging
-#
-# from ...services.slack import slack_app, builders
-# from ...models import DeviceModel, ProductModel
-#
-# log = logging.getLogger('eric')
-#
-#
-# @slack_app.command("/test")
-# def run_test_function(ack, body, client):
-# 	ack()
-# 	log.debug("test function ran")
-# 	log.debug(body)
-# 	builder = builders.DeviceAndProductViews()
-# 	builder.device = DeviceModel(3926453755)
-# 	view = builder.build_view()
-# 	log.debug(view)
-# 	client.views_open(
-# 		trigger_id=body["trigger_id"],
-# 		view=view
-# 	)
-# 	return True
+import logging
+
+from ...services.slack import slack_app, builders
+
+log = logging.getLogger('eric')
+
+
+@slack_app.command("/test")
+def run_test_function(ack, body, client):
+	ack()
+	log.debug("test function ran")
+	log.debug(body)
+	builder = builders.DeviceAndProductView()
+	modal = builders.blocks.base.get_modal_base(
+		"Test Modal",
+	)
+	modal['blocks'] = builder.create_device_and_product_blocks()
+	log.debug(modal)
+	client.views_open(
+		trigger_id=body["trigger_id"],
+		view=modal
+	)
+	return True

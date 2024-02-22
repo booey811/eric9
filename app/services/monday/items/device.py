1280f592ee9b75c5fa04162ea0447fa1173b1a8c
booey811 <gabrielbarr811@gmail.com>
2024-02-22 15:28:59 +0000

    added retreival functions to product

--- app/services/monday/items/device.py
+++ app/services/monday/items/device.py
@@ -2,6 +2,7 @@ import json
 
 from ..api.items import BaseItemType, BaseCacheableItem
 from ..api import columns
+from .product import ProductItem
 from ....utilities import notify_admins_of_error
 from ....cache import get_redis_connection
 
@@ -12,6 +13,9 @@ class DeviceItem(BaseCacheableItem):
 
 	def __init__(self, item_id=None, api_data: dict | None = None):
 		self.device_type = columns.StatusValue('status9')
+		self.products_connect = columns.ConnectBoards('connect_boards5')
+
+		self._products = None
 
 		super().__init__(item_id, api_data)
 
@@ -26,6 +30,8 @@ class DeviceItem(BaseCacheableItem):
 		return {
 			"name": self.name,
 			"id": self.id,
+			"device_type": self.device_type.value,
+			"product_ids": [str(_.id) for _ in self.products]
 		}
 
 	def load_from_cache(self, cache_data=None):
@@ -33,7 +39,20 @@ class DeviceItem(BaseCacheableItem):
 			cache_data = self.fetch_cache_data()
 		self.name = cache_data['name']
 		self.id = cache_data['id']
-		return cache_data
+		self.device_type.value = cache_data['device_type']
+		self.products_connect.value = cache_data['product_ids']
+		return self
+
+	@property
+	def products(self):
+		if self._products is None:
+			product_ids = self.products_connect.value
+			if not product_ids:
+				self._products = []
+			else:
+				self._products = ProductItem.get(product_ids)
+		return self._products
+
 
 
 

08c09b199dcaa65748bf852bb84a6ec78f87851c
booey811 <gabrielbarr811@gmail.com>
2024-02-26 18:08:13 +0000

    removed old entity viewer

--- app/services/slack/builders.py
+++ app/services/slack/builders.py
@@ -6,164 +6,6 @@ from . import blocks, exceptions
 from ..monday import items
 
 
-class DeviceAndProductView:
-
-	def __init__(self, meta: dict = None):
-		if meta is None:
-			meta = {}
-		self.meta = meta
-		self.blocks = []
-
-		self._device_id = meta.get("device_id")
-		self._product_ids = meta.get("product_ids")
-
-		self._device = None
-		self._products = None
-
-	def get_meta(self):
-		return {
-			"device_id": self._device_id,
-			"product_ids": self._product_ids
-		}
-
-	@property
-	def device(self):
-		if not self._device:
-			if self._device_id:
-				self._device = items.DeviceItem(self._device_id)
-		return self._device
-
-	@device.setter
-	def device(self, device_id: str | int):
-		self._device_id = str(device_id)
-
-	@property
-	def products(self) -> List[items.ProductItem]:
-		if not self._products:
-			if not self._product_ids:
-				self._products = []
-			else:
-				self._products = items.ProductItem.get(self._product_ids)
-		return self._products
-
-	def get_device_select_blocks(self):
-		results = []
-
-		device_data = items.DeviceItem.fetch_all(slack_data=True)
-		option_groups = blocks.objects.generate_option_groups(device_data)
-
-		static_select = blocks.elements.static_select_element(
-			placeholder="Select a device",
-			option_groups=option_groups,
-			action_id="device_select"
-		)
-
-		if self.device:
-			device_block = blocks.add.input_block(
-				block_title="Device",
-				element=static_select,
-				block_id="device_select",
-				initial_option=[self.device.name, self.device.id],
-				dispatch_action=True,
-				action_id=f"device_info"
-			)
-		else:
-			device_block = blocks.add.input_block(
-				block_title="Device",
-				element=static_select,
-				block_id="device_select",
-				dispatch_action=True,
-				action_id=f"device_info"
-			)
-		results.append(device_block)
-		return results
-
-	def get_device_view(self):
-		if not self.device:
-			raise exceptions.SlackDataError(f"Cannot view Device as no device is set. {self._device_id=}")
-
-		results = [
-			blocks.add.header_block(f"{self.device.name}"),
-			# blocks.add.simple_text_display(f"*Device Type:* {self.device.device_type.value}")
-		]
-
-		device_products = self.device.products
-		if device_products:
-			results.append(blocks.add.simple_text_display("*Related Products*"))
-			for product in device_products:
-				overflow_data = [
-					[":gear:  View Parts", f"view_parts_{product.id}"],
-					[":pound:  Adjust Price", f"adjust_price_{product.id}"],
-				]
-				overflow_options = [blocks.objects.plain_text_object(_[0], _[1]) for _ in overflow_data]
-				overflow_block = blocks.elements.overflow_accessory(f"product_overflow__{product.id}", overflow_options)
-				product_block = blocks.add.section_block(
-					title=f"{product.name}: *£{product.price}*",
-					accessory=overflow_block
-				)
-				results.append(product_block)
-
-		return results
-
-	def create_device_and_product_blocks(self):
-
-		results = []
-
-		# add device block
-		external_select = blocks.elements.external_select_element(
-			action_id="device_select",
-			placeholder="Select a device"
-		)
-		if self.device:
-			device_block = blocks.add.input_block(
-				block_title="Device",
-				element=external_select,
-				block_id="device_select",
-				initial_option=[self.device.name, self.device.id],
-				dispatch_action=True,
-				action_id="device_select"
-			)
-			results.append(device_block)
-			explanation = (
-				'Device Items describe all of the devices that we offer repairs for. Connected to each '
-				'device is a list of products, or repairs, that we offer for that device. Other pieces of '
-				'data are also connected to Devices, such as specifications for the pre-checks that should '
-				'be completed when accepting a specific device in')
-
-			text = blocks.objects.text_object(explanation)
-			results.append(blocks.add.simple_context_block([text]))
-
-		else:
-			device_block = blocks.add.input_block(
-				block_title="Device",
-				element=external_select,
-				block_id="device_select",
-				hint='Select a device to explore the entity!',
-				dispatch_action=True,
-				action_id="device_select"
-			)
-			results.append(device_block)
-
-		if self.device:
-			results.append(blocks.add.simple_text_display("*Related Products*"))
-			device_products = [_ for _ in items.ProductItem.fetch_all() if str(_.device_id) == str(self.device.id)]
-			for product in device_products:
-				overflow_data = [
-					[":gear:  View Parts", f"view_parts_{product.id}"],
-					[":pound:  Adjust Price", f"adjust_price_{product.id}"],
-				]
-				overflow_options = [blocks.objects.plain_text_object(_[0], _[1]) for _ in overflow_data]
-				overflow_block = blocks.elements.overflow_accessory(f"product_{product.id}", overflow_options)
-				product_block = blocks.add.section_block(
-					title=f"{product.name}: *£{product.price}*",
-					accessory=overflow_block
-				)
-				results.append(product_block)
-
-		p(results)
-		return results
-
-
 class EntityInformationViews:
 
 	def __init__(self):
@@ -177,6 +19,7 @@ class EntityInformationViews:
 		view_blocks.append(blocks.add.simple_text_display(f"*Related Products*"))
 		device_products = device.products
 		if device_products:
+			part_ids = []
 			for product in device_products:
 				overflow_data = [
 					[":gear:  Product Info", f"view_product"],
@@ -189,6 +32,21 @@ class EntityInformationViews:
 					accessory=overflow_block
 				)
 				view_blocks.append(product_block)
+				if product.part_ids:
+					part_ids.extend(product.part_ids)
+
+			if part_ids:
+				parts = items.PartItem.get(part_ids)
+				options = [blocks.objects.generate_option(part.name, part.id) for part in parts]
+				view_blocks.append(blocks.add.input_block(
+					block_title="*View Related Parts*",
+					element=blocks.elements.static_select_element("view_part", "Select a part", options=options),
+					block_id="view_part",
+					dispatch_action=True,
+					action_id="view_part",
+					hint='Select a part to view more information about it!'
+				))
+
 		else:
 			view_blocks.append(
 				blocks.add.simple_text_display(f"No products are connected to this device, which is odd!"))

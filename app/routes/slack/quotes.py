f4bc4d34ebbb14945c6d57c4530ee85fe4f4a60e
booey811 <gabrielbarr811@gmail.com>
2024-02-26 17:40:35 +0000

    entity explorer views are stable

--- app/routes/slack/quotes.py
+++ app/routes/slack/quotes.py
@@ -14,15 +14,12 @@ def run_test_function(ack, body, client):
 	ack()
 	log.debug("test function ran")
 	log.debug(body)
-	builder = builders.DeviceAndProductView()
-	modal_blocks = []
-	modal_blocks.extend(builder.get_device_select_blocks())
-	modal = builders.blocks.base.get_modal_base(
-		"Entity Explorer",
+	builder = builders.EntityInformationViews()
+	modal = blocks.get_modal_base(
+		"Entity Viewer",
+		submit=None
 	)
-	modal['blocks'] = modal_blocks
-	modal['private_metadata'] = json.dumps(builder.get_meta())
-	p(modal)
+	modal['blocks'] = builder.entity_view_entry_point()
 	client.views_open(
 		trigger_id=body["trigger_id"],
 		view=modal
@@ -43,8 +40,7 @@ def show_device_info(ack, body, client):
 	else:
 		raise SlackRoutingError(f"Invalid action_id for device_info action: {action_id}")
 
-	builder = builders.DeviceAndProductView()
-	builder.device = device_id
+	builder = builders.EntityInformationViews()
 
 	modal_blocks = []
 
@@ -52,17 +48,9 @@ def show_device_info(ack, body, client):
 		"Device Viewer",
 	)
 
-	explanation = (
-		'Device Items describe all of the devices that we offer repairs for. Connected to each '
-		'device is a list of products, or repairs, that we offer for that device. Other pieces of '
-		'data are also connected to Devices, such as specifications for the pre-checks that should '
-		'be completed when accepting a specific device in')
-
-	modal_blocks.append(blocks.add.simple_context_block([blocks.objects.text_object(explanation)]))
-	modal_blocks.extend(builder.get_device_view())
+	modal_blocks.extend(builder.view_device(device_id))
 
 	modal['blocks'] = modal_blocks
-	modal['private_metadata'] = json.dumps(builder.get_meta())
 	p(modal)
 	client.views_update(
 		view_id=body['view']['id'],
@@ -104,3 +92,34 @@ def show_device_info_view(ack, body, client):
 	)
 	ack()
 	return True
+
+
+@slack_app.action(re.compile("^product_overflow__.*$"))
+def respond_to_product_overview_selection(ack, client, body):
+	log.debug("product_overflow ran")
+	log.debug(body)
+	selected_action = body['actions'][0]['selected_option']['value']
+	product_id = body['actions'][0]['action_id'].split('__')[1]
+
+	if selected_action == 'view_product':
+		builder = builders.EntityInformationViews()
+
+		modal_blocks = []
+
+		modal = builders.blocks.base.get_modal_base(
+			"Product Viewer",
+		)
+
+		modal_blocks.extend(builder.view_product(product_id))
+
+		modal['blocks'] = modal_blocks
+		p(modal)
+		client.views_update(
+			view_id=body['view']['id'],
+			view=modal
+		)
+		ack()
+		return True
+
+	else:
+		raise SlackRoutingError(f"Invalid option selected for product_overflow action: {selected_action}")

5e1b74f0766800faee9fb9c1f176c69a0b7b79b8
booey811 <gabrielbarr811@gmail.com>
2024-03-01 14:24:27 +0000

    notify admins of error now dumps a text file

--- app/utilities/__init__.py
+++ app/utilities/__init__.py
@@ -1,35 +1,28 @@
 # Desc: Utility functions for the application
 
 import config
+import traceback
+import os
+import tempfile
 
 
-def notify_admins_of_error(trace):
+def notify_admins_of_error(error):
 	from ..services.slack import blocks, slack_client
 	# Integrate with an error notification tool (e.g., email, Slack, PagerDuty)
 	# This function is where you would include your custom notification logic
 
-	def make_block(content):
-		return {
-			"type": "rich_text",
-			"elements": [
-				{
-					"type": "rich_text_section",
-					"elements": [
-						{
-							"type": "text",
-							"text": content
-						}
-					]
-				}
-			]
-		}
+	# Get the stack trace
+	trace = traceback.format_exc()
 
-	s_blocks = [
-		make_block(f"An error occurred in the application: {trace}")
-	]
+	# Create a temporary file
+	with tempfile.NamedTemporaryFile(delete=False) as temp:
+		# Write the error message and stack trace to the file
+		temp.write(f"An error occurred: {error}\n\nStack trace:\n{trace}".encode())
 
-	slack_client.chat_postMessage(
-		channel=config.get_config().SLACK_ERROR_CHANNEL,
-		text='eric9:Unhandled Error',
-		blocks=s_blocks
-	)
+	# Send the file to Slack
+	with open(temp.name, 'rb') as file_content:
+		slack_client.files_upload_v2(
+			channel=config.get_config().SLACK_ERROR_CHANNEL,
+			file=file_content,
+			filename='error_log.txt'
+		)

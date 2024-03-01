8f1230c35b4cb906e1a5a78eb9fe0c82bc81c31e
booey811 <gabrielbarr811@gmail.com>
2024-03-01 14:32:35 +0000

    added save_metadata for slack

--- app/services/slack/exceptions.py
+++ app/services/slack/exceptions.py
@@ -1,5 +1,18 @@
 from ...errors import EricError
 
+from ...cache import get_redis_connection
+
+
+def save_metadata(meta, key_name):
+	"""saves current app metadata to cache for retrieval if needed"""
+	key = f"slack_meta:{key_name}"
+	get_redis_connection().set(
+		name=key,
+		value=meta,
+		ex=86400  # 1 day
+	)
+	return True
+
 
 class SlackDataError(EricError):
 

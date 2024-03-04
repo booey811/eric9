28ef053be46f05480823c6aace4cb37787d20de5
booey811 <gabrielbarr811@gmail.com>
2024-03-04 14:25:08 +0000

    added parts and pre-checks to cache

--- app/tasks/recurring/build_cache.py
+++ app/tasks/recurring/build_cache.py
@@ -1,6 +1,8 @@
 # run_cache.py
-from app.cache.utilities import build_product_cache, build_device_cache
+from app.cache.utilities import build_product_cache, build_device_cache, build_pre_check_cache, build_part_cache
 
 if __name__ == "__main__":
     build_product_cache()
     build_device_cache()
+    build_pre_check_cache()
+    build_part_cache()

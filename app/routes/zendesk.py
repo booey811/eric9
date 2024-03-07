5c6f449d36dafeec6fe423500c3011eabe2c9f16
booey811 <gabrielbarr811@gmail.com>
2024-03-07 12:41:39 +0000

    added routing for syncing from Zendesk

---
+++ app/routes/zendesk.py
@@ -0,0 +1,33 @@
+import json
+
+from flask import Blueprint, request, jsonify
+
+from ..cache.rq import q_high
+from ..services import zendesk
+from ..tasks import sync_platform
+
+import config
+
+conf = config.get_config()
+
+zendesk_bp = Blueprint('zendesk', __name__, url_prefix='/zendesk')
+
+
+@zendesk_bp.route('/index', methods=['GET'])
+def zendesk_creates_monday_ticket():
+	data = request.get_data().decode()
+	data = json.loads(data)
+	ticket_id = data['id']
+	event = data['event']
+
+	if event == 'sync_item':
+		if conf.CONFIG in ('DEVELOPMENT', 'TESTING'):
+			sync_platform.sync_to_monday(ticket_id)
+		else:
+			q_high.enqueue(
+				sync_platform.sync_to_monday(ticket_id)
+			)
+	else:
+		raise ValueError(f"Invalid event: {event}")
+
+	return jsonify({'status': 'success'}, 200)

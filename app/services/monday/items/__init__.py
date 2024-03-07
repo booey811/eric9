fb451746b8afd4baf31a8df540f09652329ee2e9
booey811 <gabrielbarr811@gmail.com>
2024-03-07 19:36:07 +0000

    arrangd imports

--- app/services/monday/items/__init__.py
+++ app/services/monday/items/__init__.py
@@ -1,5 +1,5 @@
 from .main_item import MainItem
 from .product import ProductItem
-from . import misc, repair_phases, part
+from . import misc, repair_phases, part, corporate
 from.device import DeviceItem
 from .part import PartItem

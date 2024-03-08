e55eb4a5aea0ada6be17ed36a0e4ee01f5fde78f
booey811 <gabrielbarr811@gmail.com>
2024-03-08 17:19:05 +0000

    added pagination to get_api_items

--- app/services/monday/api/client.py
+++ app/services/monday/api/client.py
@@ -12,18 +12,26 @@ conn = monday.MondayClient(conf.MONDAY_KEYS["gabe"])
 
 
 def get_api_items(item_ids):
+	item_data = []
 	item_ids = [int(_) for _ in item_ids]
 	if not item_ids:
 		return []
-	try:
-		raw = conn.items.fetch_items_by_id(ids=item_ids)
-	except Exception as e:
-		raise MondayAPIError(f"Error calling monday API: {e}")
 
-	if raw.get("error_message"):
-		raise MondayAPIError(f"Error fetching items from Monday: {raw['error_message']}")
+	# Slice item_ids into blocks of 25
+	item_id_blocks = [item_ids[i:i + 25] for i in range(0, len(item_ids), 25)]
 
-	return raw["data"]["items"]
+	for item_id_block in item_id_blocks:
+		try:
+			query_result = conn.items.fetch_items_by_id(ids=item_id_block)
+		except Exception as e:
+			raise MondayAPIError(f"Error calling monday API: {e}")
+
+		if query_result.get("error_message"):
+			raise MondayAPIError(f"Error fetching items from Monday: {query_result['error_message']}")
+
+		item_data.extend(query_result["data"]["items"])
+
+	return item_data
 
 
 def get_api_items_by_group(board_id, group_id):

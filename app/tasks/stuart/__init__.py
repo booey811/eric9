cea72acccdcdd4eadb887fca108b61b4a17b8133
Gabriel Barr <gabrielbarr811@gmail.com>
2024-03-14 17:53:22 +0000

    added courier data dumpm task

--- app/tasks/stuart/__init__.py
+++ app/tasks/stuart/__init__.py
@@ -1,4 +1,8 @@
+import datetime
+import json
+
 from ...services import monday, stuart
+from ...utilities import notify_admins_of_error
 
 
 def book_collection(main_id):
@@ -11,5 +15,47 @@ def book_collection(main_id):
 		main_item.be_courier_collection = 'Error'
 		main_item.commit()
 		return main_item
+	try:
+		response = stuart.client.create_job(job_data)
+		response = response.json()
+	except Exception as e:
+		main_item.add_update(f"Could not create job: {e}", main_item.error_thread_id)
+		main_item.be_courier_collection = 'Error'
+		main_item.commit()
+		return main_item
 
-	stuart.client.create_job(job_data)
+	log_job_data(job_data, response, main_item)
+	return response
+
+
+def log_job_data(booking_data, stuart_response, main_item):
+	"""Log the job data"""
+
+	try:
+		log_item = monday.items.misc.CourierDataDumpItem()
+
+		log_item.job_id = str(stuart_response['id'])
+		# if main_item.booking_date.value:
+		# 	log_item.booking_time = main_item.booking_date.value
+		# else:
+		# 	log_item.booking_time = datetime.datetime.now()
+
+		log_item.cost_inc_vat = stuart_response['pricing']['price_tax_included']
+		log_item.cost_ex_vat = stuart_response['pricing']['price_tax_excluded']
+		log_item.vat = stuart_response['pricing']['tax_amount']
+
+		log_item.collection_postcode = stuart_response['deliveries'][0]['pickup']['address']['postcode']
+		log_item.delivery_postcode = stuart_response['deliveries'][0]['dropoff']['address']['postcode']
+		log_item.distance = stuart_response['distance']
+		log_item.tracking_url = ["Tracking", stuart_response['deliveries'][0]['tracking_url']]
+		log_item.main_item_id = str(main_item.id)
+
+		log_item.create(main_item.name)
+
+		log_item.add_update(json.dumps(booking_data, indent=4))
+
+		return log_item
+
+	except Exception as e:
+		notify_admins_of_error(e)
+		raise e

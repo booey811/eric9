a1cb6b585fb440a0d972421cc0241c490cf99aa9
booey811 <gabrielbarr811@gmail.com>
2024-03-19 11:55:55 +0000

    added sales classes

---
+++ app/services/monday/items/sales.py
@@ -0,0 +1,28 @@
+from ..api.items import BaseItemType
+from ..api import columns
+
+
+class SaleControllerItem(BaseItemType):
+
+	BOARD_ID = 6285416596
+
+	def __init__(self, item_id: int, api_data=None, search=None, cache_data=None):
+		self.main_item_id = columns.TextValue("text")
+		self.main_item_connect = columns.ConnectBoards("connect_boards")
+
+		self.processing_status = columns.StatusValue("status4")
+
+		super().__init__(item_id, api_data, search, cache_data)
+
+
+class SaleLineItem(BaseItemType):
+
+	BOARD_ID = 6285426254
+
+	def __init__(self, item_id: int, api_data=None, search=None, cache_data=None):
+
+		self.source_id = columns.TextValue("text")
+		self.line_type = columns.StatusValue("status2")
+		self.price_inc_vat = columns.NumberValue("numbers")
+
+		super().__init__(item_id, api_data, search, cache_data)

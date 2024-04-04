3c5a16c1cf537e6a5bf2d7c068b4876119ad50f6
booey811 <gabrielbarr811@gmail.com>
2024-04-04 15:06:11 +0100

    correction for sickW main item search

--- main.py
+++ main.py
@@ -10,15 +10,14 @@ port = int(os.environ.get('PORT', 8000))  # Default to 8000 for local developmen
 
 eric = app.create_app()
 
-
 if __name__ == '__main__':
 	if env == 'production':
 		eric.run()
 	elif env == 'testing':
 		eric.run()
 	elif env == 'development':
-		from app.tasks.monday import stock_control
-		stock_control.update_stock_checkouts(4819915342)
-		stock_control.process_stock_checkout(6278365602)
+		from app.services import monday
+		from app.utilities import tools
+		tools.MondayTools.list_redundant_columns(985177480)
 	else:
 		raise Exception(f"Invalid ENV: {env}")

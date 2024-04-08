97cb4c94df20ad3cb8298333ecdd84a7e0c00a2f
booey811 <gabrielbarr811@gmail.com>
2024-04-08 18:23:13 +0100

    removed redundant attributes from InventoryAdjustments

--- app/services/monday/items/part.py
+++ app/services/monday/items/part.py
@@ -136,11 +136,11 @@ class InventoryAdjustmentItem(BaseItemType):
 		self.part_id = columns.TextValue("text4")
 		self.part_url = columns.LinkURLValue("part_url")
 		
-		self.parts_connect = columns.ConnectBoards("connect_boards9")
-		self.supplier_connect = columns.ConnectBoards("connect_boards")
+		# self.parts_connect = columns.ConnectBoards("connect_boards9")
+		# self.supplier_connect = columns.ConnectBoards("connect_boards")
 
-		self.auto_order_status = columns.StatusValue("status_1")
-		self.auto_order_minimum = columns.NumberValue("numbers")
+		# self.auto_order_status = columns.StatusValue("status_1")
+		# self.auto_order_minimum = columns.NumberValue("numbers")
 
 		self.source_item_id = columns.TextValue("mainboard_id")
 		self.source_url = columns.LinkURLValue("link2")

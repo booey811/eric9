30044d42734a76dca263b05aec479586fe19b4e3
booey811 <gabrielbarr811@gmail.com>
2024-04-12 17:43:23 +0100

    added review function

--- app/tasks/monday/misc.py
+++ app/tasks/monday/misc.py
@@ -1,6 +1,6 @@
 import json
 
-from ...services import monday, sickw
+from ...services import monday, sickw, zendesk
 from ...utilities import notify_admins_of_error
 
 
@@ -78,7 +78,8 @@ def handle_imei_change(main_item_id):
 		)
 
 		if not main_board_imei_search.get('error_message'):
-			main_ids_from_board = [item['id'] for item in main_board_imei_search['data']['items_page_by_column_values']['items']]
+			main_ids_from_board = [item['id'] for item in
+								   main_board_imei_search['data']['items_page_by_column_values']['items']]
 			main_ids.extend([str(item) for item in main_ids_from_board])
 
 		if main_ids:
@@ -92,7 +93,6 @@ def handle_imei_change(main_item_id):
 
 
 def print_battery_test_results_to_main_item(battery_test_item_id):
-
 	battery_test_item = monday.items.misc.BatteryTestItem(battery_test_item_id)
 
 	try:
@@ -101,7 +101,8 @@ def print_battery_test_results_to_main_item(battery_test_item_id):
 			if not main_id:
 				raise IndexError
 		except IndexError:
-			raise monday.api.exceptions.MondayDataError(f"No Main Item Connected to Battery Test Item: {battery_test_item_id}")
+			raise monday.api.exceptions.MondayDataError(
+				f"No Main Item Connected to Battery Test Item: {battery_test_item_id}")
 		main_item = monday.items.MainItem(main_id)
 
 		# get the battery test data
@@ -131,4 +132,54 @@ def print_battery_test_results_to_main_item(battery_test_item_id):
 		battery_test_item.test_status = "Error"
 		battery_test_item.commit()
 		battery_test_item.add_update(f"Error Printing Battery Test Results to Main Item: {str(e)}")
+		raise e
+
+
+def request_client_feedback(main_item_id):
+	feedback_request_zendesk_strings = [
+		["requested", "feedback_request-1"],
+		["not_requested", "feedback_request-5"],
+		["do_not_request", "feedback_request-19"]
+	]
+
+	main_item = monday.items.MainItem(main_item_id)
+	try:
+		if not main_item.ticket_id.value:
+			raise monday.api.exceptions.MondayDataError(f"No Ticket ID Provided for {main_item}")
+		# get zendesk ticket
+		ticket = zendesk.client.tickets(id=int(main_item.ticket_id.value))
+		user = ticket.requester
+		current_feedback_status = user.user_fields['review_requests']
+		if current_feedback_status:
+			try:
+				human_readable_status = [x[0] for x in feedback_request_zendesk_strings if x[1] == current_feedback_status][
+					0]
+			except IndexError:
+				raise monday.api.exceptions.MondayDataError(f"Invalid Feedback Request Status: {current_feedback_status}")
+		else:
+			human_readable_status = "not_requested"
+
+		if human_readable_status == 'requested':
+			main_item.add_update(f"Feedback Already Requested for {user.name}", main_item.notes_thread_id.value)
+			return main_item
+		elif human_readable_status == 'do_not_request':
+			main_item.add_update(f"Feedback Request Disabled for {user.name}", main_item.notes_thread_id.value)
+			return main_item
+		elif human_readable_status == 'not_requested':
+			# actually send the request
+			pass
+		else:
+			raise monday.api.exceptions.MondayDataError(f"Invalid Feedback Request Status: {current_feedback_status}")
+
+		macro_id = 1900008072173
+		effect = zendesk.client.tickets.show_macro_effect(ticket, macro_id)
+		zendesk.client.tickets.update(effect.ticket)
+		main_item.add_update(f"Feedback Requested for {user.name}", main_item.notes_thread_id.value)
+		user.user_fields['review_requests'] = "feedback_request-1"
+		zendesk.client.users.update(user)
+
+		return main_item
+	except Exception as e:
+		main_item.add_update(f"Error Requesting Feedback: {str(e)}", main_item.error_thread_id.value)
+		notify_admins_of_error(f"Error Requesting Feedback: {str(e)}")
 		raise e

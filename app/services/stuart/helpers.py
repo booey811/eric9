2100f7fb8079b676a0e404bd15bb53a8d2eb21d5
booey811 <gabrielbarr811@gmail.com>
2024-04-15 14:03:46 +0100

    adjusted stuart for DST, again

--- app/services/stuart/helpers.py
+++ app/services/stuart/helpers.py
@@ -20,7 +20,8 @@ def generate_address_string(address_notes, address_street, address_postcode):
 
 def generate_job_data(main_board_item: monday.items.MainItem, direction, client_reference=""):
 	if main_board_item.booking_date.value:
-		dt = main_board_item.booking_date.value.astimezone(pytz.timezone('Europe/London'))
+		dt = main_board_item.booking_date.value
+		dt = dt.astimezone(pytz.timezone('Europe/London'))
 		time = dt.isoformat()
 	else:
 		time = None

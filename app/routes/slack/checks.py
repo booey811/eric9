db0e7158dfc99290f15b0c005053d7ee60c250ac
booey811 <gabrielbarr811@gmail.com>
2024-04-18 18:22:32 +0100

    WIP - check submission

--- app/routes/slack/checks.py
+++ app/routes/slack/checks.py
@@ -77,8 +77,74 @@ def test_route_for_checks(ack, body, client):
 
 @slack_app.view("checks_form")
 def checks_form_submission(ack, body, client):
+	# SAMPLE SUBMISSION DATA
+	SUB_DATA = {'4455646219': {'check_action__4455646219': {'selected_option': {'text': {'emoji': True,
+																						 'text': 'Glass '
+																								 'Damage',
+																						 'type': 'plain_text'},
+																				'value': 'Glass '
+																						 'Damage'},
+															'type': 'static_select'}},
+				'4455646235': {'check_action__4455646235': {'selected_option': {'text': {'emoji': True,
+																						 'text': 'Damaged',
+																						 'type': 'plain_text'},
+																				'value': 'Damaged'},
+															'type': 'static_select'}},
+				'4455646244': {'check_action__4455646244': {'selected_option': {'text': {'emoji': True,
+																						 'text': 'No '
+																								 'Damage',
+																						 'type': 'plain_text'},
+																				'value': 'No '
+																						 'Damage'},
+															'type': 'static_select'}},
+				'4455683287': {'check_action__4455683287': {'selected_option': {'text': {'emoji': True,
+																						 'text': 'Blurry '
+																								 'Image',
+																						 'type': 'plain_text'},
+																				'value': 'Blurry '
+																						 'Image'},
+															'type': 'static_select'}},
+				'4455686584': {'check_action__4455686584': {'selected_option': {'text': {'emoji': True,
+																						 'text': 'Working',
+																						 'type': 'plain_text'},
+																				'value': 'Working'},
+															'type': 'static_select'}},
+				'4455689088': {'check_action__4455689088': {'selected_options': [{'text': {'emoji': True,
+																						   'text': 'Not '
+																								   'Working',
+																						   'type': 'plain_text'},
+																				  'value': 'Not '
+																						   'Working'}],
+															'type': 'multi_static_select'}},
+				'4455696563': {'check_action__4455696563': {'selected_option': {'text': {'emoji': True,
+																						 'text': 'Below '
+																								 '80',
+																						 'type': 'plain_text'},
+																				'value': 'Below '
+																						 '80'},
+															'type': 'static_select'}},
+				'6437392783': {'check_action__6437392783': {'type': 'plain_text_input',
+															'value': '12'}},
+				'6437393469': {'check_action__6437393469': {'type': 'number_input',
+															'value': '5'}},
+				'6437395630': {'check_action__6437395630': {'selected_option': {'text': {'emoji': True,
+																						 'text': 'Yes',
+																						 'type': 'plain_text'},
+																				'value': 'Yes'},
+															'type': 'static_select'}},
+				'6437398898': {'check_action__6437398898': {'type': 'number_input',
+															'value': '12'}},
+				'6437587112': {'check_action__6437587112': {'selected_option': {'text': {'emoji': True,
+																						 'text': 'No',
+																						 'type': 'plain_text'},
+																				'value': 'No'},
+															'type': 'static_select'}}}
+
 	from pprint import pprint as p
-	ack()
+
 	flow = flows.ChecksFlow(client, ack, body)
+	for check_id in body['view']['state']['values']:
+		answer = body['view']['state']['values'][check_id]['check_action__' + check_id]['selected_option']['value']
+
 	p(body)
 	return True

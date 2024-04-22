b6eeba7463f2bc36a09ae0745ec55df17fcac98a
booey811 <gabrielbarr811@gmail.com>
2024-04-22 18:35:26 +0100

    added task for printing check results to main item

--- app/tasks/monday/repair_process.py
+++ app/tasks/monday/repair_process.py
@@ -1,8 +1,8 @@
+from ...utilities import notify_admins_of_error
 from ...services import monday as mon_obj
 
 
 def sync_check_items_and_results_columns():
-
 	results_board_id = 6487504495
 	results_sub_board_id = 6487518414
 	check_board_id = 4455646189
@@ -42,3 +42,29 @@ def sync_check_items_and_results_columns():
 		# if the column is present, continue
 		continue
 
+
+def print_results_main_item(main_item_id, results_subitem_id):
+	main_item = mon_obj.items.MainItem(main_item_id)
+	try:
+		raw_col_data = mon_obj.api.monday_connection.boards.fetch_columns_by_board_id(6487518414)['data']['boards'][0][
+			'columns']
+		col_data = {col['id']: col['title'] for col in raw_col_data}
+		raw_results_data = mon_obj.api.get_api_items([results_subitem_id])[0]['column_values']
+		results_data = {col['id']: col['text'] for col in raw_results_data}
+		results_formatted = [
+			(col_data.get(col_id), results_data.get(col_id)) for col_id in results_data.keys()
+		]
+
+		update = "==== CHECKS RESULTS ===="
+		update += f"\n{results_data['date__1']}\n\n"
+
+		for q, a in results_formatted:
+			if q != "Date Submitted" and a:
+				update += f"\n<b>{q}</b>: {a}"
+
+		main_item.add_update(update, main_item.notes_thread_id.value)
+		return main_item
+	except Exception as e:
+		main_item.add_update(f"Could Not Print Device Check Data: {e}", main_item.notes_thread_id.value)
+		notify_admins_of_error(f"Could Not Print Device Check Data: {e}")
+		raise e

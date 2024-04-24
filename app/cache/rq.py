30ddc6dfd869899385eee3612c8b6b1fd22876dc
booey811 <gabrielbarr811@gmail.com>
2024-04-24 11:47:53 +0100

    added failure_ttl to redis

--- app/cache/rq.py
+++ app/cache/rq.py
@@ -10,10 +10,10 @@ a_sync = True
 # if conf.DEBUG:
 #     a_sync = False
 
-q_low = Queue('low', connection=get_redis_connection(), is_async=a_sync)
-q_med = Queue('medium', connection=get_redis_connection(), is_async=a_sync)
-q_high = Queue('high', connection=get_redis_connection(), is_async=a_sync)
-q_ai_results = Queue('ai_results', connection=get_redis_connection(), is_async=a_sync)
+q_low = Queue('low', connection=get_redis_connection(), is_async=a_sync, failure_ttl=500)
+q_med = Queue('medium', connection=get_redis_connection(), is_async=a_sync, failure_ttl=500)
+q_high = Queue('high', connection=get_redis_connection(), is_async=a_sync, failure_ttl=500)
+q_ai_results = Queue('ai_results', connection=get_redis_connection(), is_async=a_sync, failure_ttl=500)
 
 
 def remove_failed_jobs():

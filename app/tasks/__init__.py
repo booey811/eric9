e49c57cc726530c7a8d1e3302b0c6bf650261643
booey811 <gabrielbarr811@gmail.com>
2024-05-15 18:08:23 +0100

    raised zenesk tasks to module level

--- app/tasks/__init__.py
+++ app/tasks/__init__.py
@@ -1,1 +1,1 @@
-from . import monday, notifications, notion, recurring, scheduling, sync_platform, slack, stuart
+from . import monday, notifications, notion, recurring, scheduling, sync_platform, slack, stuart, zendesk

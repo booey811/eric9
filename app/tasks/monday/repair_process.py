8d75ecc2fd85d17d5a97042aa463e88e97f59538
booey811 <gabrielbarr811@gmail.com>
2024-06-21 17:10:56 +0100

    forced check item storage creation to fetch full api data

--- app/tasks/monday/repair_process.py
+++ app/tasks/monday/repair_process.py
@@ -14,6 +14,7 @@ def sync_check_items_and_results_columns():
 
 	# get all the items on the check item board
 	item_data = mon_obj.api.get_items_by_board_id(check_board_id)  # Checks Items Board ID
+	check_item_data = mon_obj.api.get_api_items([i['id'] for i in item_data])  # Check Results Board ID
 	check_items = [mon_obj.items.misc.CheckItem(_['id'], _) for _ in item_data]
 
 	# for each item, check the results_column_id field

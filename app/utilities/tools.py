95735781d8aa8b81aef5341c50d6e620421cab0f
booey811 <gabrielbarr811@gmail.com>
2024-06-27 16:53:21 +0100

    added historic etl functions to tools

--- app/utilities/tools.py
+++ app/utilities/tools.py
@@ -6,6 +6,7 @@ import html2text
 
 from ..services.monday import items as items_package, api as api_package
 from ..services.monday.api.columns import ValueType
+from ..services import monday
 
 
 class MondayTools:
@@ -68,3 +69,50 @@ class MondayTools:
 				final = f"Note from User({update['creator']['id']})\nTimestamp:{update['created_at']}\n\n{text}\n====================\n\n"
 				f.write(final)
 		return file_path
+
+
+class ETLFunctions:
+
+	@staticmethod
+	def etl_device_type_in_sales():
+		api_data = monday.api.get_items_by_board_id(6285416596)
+		sales = [monday.items.sales.SaleControllerItem(_['id'], _) for _ in api_data]
+		for sale in sales:
+			try:
+				main = sale.get_main_item()
+				if main.device_id:
+					device = monday.items.device.DeviceItem(main.device_id)
+					device_type = device.device_type.value
+				else:
+					device_type = "Other Device"
+				sale.device_type = device_type
+				sale.commit()
+			except Exception as e:
+				sale.device_type = "Unconfirmed"
+				print(str(e))
+
+	@staticmethod
+	def etl_parts_costs_to_sales():
+		api_data = monday.api.get_items_by_board_id(6267736041)  # stock checkout board
+		checkout_items = [monday.items.part.StockCheckoutControlItem(_['id'], _) for _ in api_data]
+		for ci in checkout_items:
+			try:
+				main_id = ci.main_item_id.value
+				if ci.checkout_line_ids.value:
+					line_item_data = monday.api.get_api_items(ci.checkout_line_ids.value)
+					line_items = [monday.items.part.StockCheckoutLineItem(_['id'], _) for _ in line_item_data]
+					parts_cost = sum(float(_.parts_cost.value) for _ in line_items if _.parts_cost.value)
+				else:
+					raise Exception(f"No Parts Cost Data Attached for {ci.name}")
+				sale_data = monday.items.sales.SaleControllerItem().search_board_for_items(
+					"main_item_id", str(main_id)
+				)[0]
+
+				sale_item = monday.items.sales.SaleControllerItem(sale_data['id'], sale_data)
+				sale_item.parts_cost = parts_cost
+				sale_item.commit()
+			except Exception as e:
+				print(f"Error: {str(e)}")
+				ci.etl_to_sale = "f"
+				ci.commit()
+				continue
